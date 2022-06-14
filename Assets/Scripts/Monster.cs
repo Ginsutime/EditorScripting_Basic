99e515d013c54438c0300aee3c050b2b4e75ca3c
Brett <ginsutime78@gmail.com>
2022-06-14 16:05:37 -0500

    Basic runthrough of attributes, enums and scriptable objects

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] private MonsterData _data;
+
+    public MonsterData Data => _data;
+
+    private void Awake()
+    {
+        Debug.Log("Name: " + _data.Name);
+        Debug.Log("Name: " + _data.Damage);
+    }
+}
