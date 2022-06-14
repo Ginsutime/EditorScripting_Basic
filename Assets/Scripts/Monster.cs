ab67971a83302ca29f4d635125e35349be7b01f9
Brett <ginsutime78@gmail.com>
2022-06-14 16:50:24 -0500

    Visual debugging intro using gizmos

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -13,4 +13,16 @@ public class Monster : MonoBehaviour
         Debug.Log("Name: " + _data.Name);
         Debug.Log("Name: " + _data.Damage);
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _data.RangeOfAwareness);
+
+        Gizmos.color = Color.cyan;
+        Vector3 ray = transform.forward * _data.RangeOfAwareness;
+        Gizmos.DrawRay(transform.position, ray);
+    }
+#endif
 }
