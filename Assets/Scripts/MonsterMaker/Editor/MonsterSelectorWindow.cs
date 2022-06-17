0a8372d09901c22d50029d720ae96f73d4b4ae81
Brett <ginsutime78@gmail.com>
2022-06-16 19:24:13 -0500

    Added ex functionality to selector window (Select All of Monster Type)

--- Assets/Scripts/MonsterMaker/Editor/MonsterSelectorWindow.cs
+++ Assets/Scripts/MonsterMaker/Editor/MonsterSelectorWindow.cs
@@ -20,5 +20,29 @@ public class MonsterSelectorWindow : EditorWindow
         GUILayout.Label("Selection Filters:", EditorStyles.boldLabel);
         _selectedMonsterType = (MonsterType)EditorGUILayout.EnumPopup(
             "MonsterType to select:", _selectedMonsterType);
+        EditorGUILayout.Space(10);
+
+        if (GUILayout.Button("Select All"))
+        {
+            SelectAllMonsters();
+        }
+    }
+
+    private void SelectAllMonsters()
+    {
+        // Collect all monsters in scene
+        Monster[] monsters = FindObjectsOfType<Monster>();
+        // Create temporary list to store valid monsters as we check
+        List<GameObject> finalSelection = new List<GameObject>();
+        // Check each monster, store if type matches
+        foreach (Monster monster in monsters)
+        {
+            if (monster.Data.MonsterType == _selectedMonsterType)
+            {
+                finalSelection.Add(monster.gameObject);
+            }
+        }
+        // Create a selection from valid monsters
+        Selection.objects = finalSelection.ToArray();
     }
 }
